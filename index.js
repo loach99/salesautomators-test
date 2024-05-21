75895d0e9d8c04ef451e20aed3a07063ef69b288
Konstantin Vyunkov <constantinent@mail.ru>
2024-05-21 17:20:27 +0300

    test api

--- index.js
+++ index.js
@@ -1,83 +1,95 @@
 const API = '9d1c35db4f1a8bd32f12fd15085ded2aa131e634';
 const express = require('express');
-const path = require('path');
-const passport = require('passport');
-const OAuth2Strategy = require('passport-oauth').OAuth2Strategy;
 
 const api = require('./api');
 const config = require('./config');
+const axios = require('axios');
 
-require('https').globalAgent.options.rejectUnauthorized = false;
 
 
 const app = express();
 const port = 3000;
 
-passport.use(
-	'pipedrive',
-	new OAuth2Strategy({
-			authorizationURL: 'https://oauth.pipedrive.com/oauth/authorize',
-			tokenURL: 'https://oauth.pipedrive.com/oauth/token',
-			clientID: config.clientID || '',
-			clientSecret: config.clientSecret || '',
-			callbackURL: config.callbackURL || ''
-		}, async (accessToken, refreshToken, profile, done) => {
-			const userInfo = await api.getUser(accessToken);
-			const user = await User.add(
-				userInfo.data.name,
-				accessToken,
-				refreshToken
-			);
-
-			done(null, { user });
-		}
-	)
-);
-
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(passport.initialize());
-app.use(async (req, res, next) => {
-	req.user = await User.getById(1);
-	next();
-});
-
-app.get('/auth/pipedrive', passport.authenticate('pipedrive'));
-app.get('/auth/pipedrive/callback', passport.authenticate('pipedrive', {
-    session: false,
-    failureRedirect: '/',
-    successRedirect: '/'
-}));
-app.get('/', async (req, res) => {
-    if (req.user.length < 1) {
-        return res.redirect('/auth/pipedrive');
-    }
-
+app.get('/getData', async (req, res)=>{
     try {
-        const deals = await api.getDeals(req.user[0].access_token);
-
-        res.render('deals', {
-            name: req.user[0].username,
-            deals: deals.data
-        });
+        const response = await axios.get('https://oauth.pipedrive.com/oauth/authorize',{
+            params:{
+                client_id: '6e1054f640335df4',
+                redirect_uri: 'https://main--pipedriveloach99.netlify.app/'
+            }
+        })
+        res.send(response.data)
     } catch (error) {
-        return res.send(error.message);
+        console.error(error);
+        res.status(500).send('An error occurred');
     }
-});
+})
 
-app.get('/deals/:id', async (req, res) => {
-    const randomBoolean = Math.random() >= 0.5;
-    const outcome = randomBoolean === true ? 'won' : 'lost';
+// passport.use(
+// 	'pipedrive',
+// 	new OAuth2Strategy({
+// 			authorizationURL: 'https://oauth.pipedrive.com/oauth/authorize',
+// 			tokenURL: 'https://oauth.pipedrive.com/oauth/token',
+// 			clientID: config.clientID || '',
+// 			clientSecret: config.clientSecret || '',
+// 			callbackURL: config.callbackURL || ''
+// 		}, async (accessToken, refreshToken, profile, done) => {
+// 			const userInfo = await api.getUser(accessToken);
+// 			const user = await User.add(
+// 				userInfo.data.name,
+// 				accessToken,
+// 				refreshToken
+// 			);
 
-    try {
-        await api.updateDeal(req.params.id, outcome, req.user[0].access_token);
+// 			done(null, { user });
+// 		}
+// 	)
+// );
 
-        res.render('outcome', { outcome });
-    } catch (error) {
-        return res.send(error.message);
-    }
-});
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'hbs');
 
-app.listen(port, () => console.log(`🟢 App has started. \n🔗 Live URL: https://${process.env.PROJECT_DOMAIN}.glitch.me`));
+// app.use(express.static(path.join(__dirname, 'public')));
+// app.use(passport.initialize());
+// app.use(async (req, res, next) => {
+// 	req.user = await User.getById(1);
+// 	next();
+// });
+
+// app.get('/auth/pipedrive', passport.authenticate('pipedrive'));
+// app.get('/auth/pipedrive/callback', passport.authenticate('pipedrive', {
+//     session: false,
+//     failureRedirect: '/',
+//     successRedirect: '/'
+// }));
+// app.get('/', async (req, res) => {
+//     if (req.user.length < 1) {
+//         return res.redirect('/auth/pipedrive');
+//     }
+
+//     try {
+//         const deals = await api.getDeals(req.user[0].access_token);
+
+//         res.render('deals', {
+//             name: req.user[0].username,
+//             deals: deals.data
+//         });
+//     } catch (error) {
+//         return res.send(error.message);
+//     }
+// });
+
+// app.get('/deals/:id', async (req, res) => {
+//     const randomBoolean = Math.random() >= 0.5;
+//     const outcome = randomBoolean === true ? 'won' : 'lost';
+
+//     try {
+//         await api.updateDeal(req.params.id, outcome, req.user[0].access_token);
+
+//         res.render('outcome', { outcome });
+//     } catch (error) {
+//         return res.send(error.message);
+//     }
+// });
+
+app.listen(port, () => console.log(`🟢 App has started.`));
